63c267339dae74b58072447f6f0886edf162fed9
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-05-17 14:21:39 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CS_0510
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mybutton = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // mybutton
+            // 
+            mybutton.BackColor = Color.FromArgb(0, 192, 0);
+            mybutton.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            mybutton.Location = new Point(434, 52);
+            mybutton.Name = "mybutton";
+            mybutton.Size = new Size(193, 174);
+            mybutton.TabIndex = 0;
+            mybutton.Text = "押すなよ??";
+            mybutton.UseVisualStyleBackColor = false;
+            mybutton.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(133, 254);
+            button2.Name = "button2";
+            button2.Size = new Size(244, 165);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(mybutton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button mybutton;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+namespace CS_0510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mybutton.Text = "‰Ÿ‚³‚ê‚½";
+            mybutton.Left = 0;
+            //button1.Visible = false;
+            MessageBox.Show("‰½Œ©‚Ä‚ñ‚¾(^-^#) ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
